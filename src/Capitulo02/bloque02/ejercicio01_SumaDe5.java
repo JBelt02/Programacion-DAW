7f36e13cef0fccadffe25939e9419a2cde94317c
JBelt02 <granjuan02@gmail.com>
2022-09-27 11:29:34 +0200

    feat (finishing ej1 from chap2 brick 02)

--- src/Capitulo02/bloque02/ejercicio01_SumaDe5.java
+++ src/Capitulo02/bloque02/ejercicio01_SumaDe5.java
@@ -15,7 +15,24 @@ public class ejercicio01_SumaDe5 {
 		int num4 = sc.nextInt();
 		System.out.println("Introduzca un numero : ");
 		int num5 = sc.nextInt();
+		int suma = 0;
 		
+		if(num1<0) {
+			int neg1=0 ;
+			num1=neg1 ; 
+		}else if(num1>=0 && num1<=25) {
+			int baj1=0;
+			num1=baj1;
+		}else if (num1>=26 && num1<=250) {
+			int med1=0;
+			num1=med1;
+		}else if (num1>250) {
+			int grand1=0;
+			num1=grand1;
+		}
 		
+		switch (suma) {
+		case 1 :
+		}
 	}
 }
