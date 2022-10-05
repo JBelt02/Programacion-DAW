94be2c06d7d98938c854b1c975fee51aae7c03c0
JBelt02 <granjuan02@gmail.com>
2022-10-05 09:56:29 +0200

    feat(Finished all activities from chap03 block02)

--- src/Capitulo03/bloque02/Ejercicio04_IgualQueElAnteriorPeroTuPonesElLimite.java
+++ src/Capitulo03/bloque02/Ejercicio04_IgualQueElAnteriorPeroTuPonesElLimite.java
@@ -2,14 +2,15 @@ package Capitulo03.bloque02;
 
 import java.util.Scanner;
 
-public class Ejercicio03_MultiplosDeUnNumero {
-
+public class Ejercicio04_IgualQueElAnteriorPeroTuPonesElLimite {
 	public static void main(String[] args) {
 		Scanner sc =new Scanner (System.in);
 		System.out.println("Introduzca un numero ");
-		int num = sc.nextInt();	
+		int num = sc.nextInt();
+		System.out.println("Introduzca el limite de los multiplos");
+		int limit = sc.nextInt();
 		
-		for (int i = 0; (i * num) < 100; i++) {
+		for (int i = 0; (i * num) < limit; i++) {
 			System.out.println("Multiplos de : "+num+ " son: " + (i * num));
 		}
 	}
