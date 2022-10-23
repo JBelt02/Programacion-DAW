ce44fa4fab28ce5f792c0ce65dc31c9dc2005c90
JBelt02 <granjuan02@gmail.com>
2022-10-23 20:40:50 +0200

    Feat (finished all activities from chap 04 block 02)

--- src/Capitulo04/bloque02/Ejercicio06_DesplazamientoEnSentidoContrario.java
+++ src/Capitulo04/bloque02/Ejercicio06_DesplazamientoEnSentidoContrario.java
@@ -4,7 +4,14 @@ public class Ejercicio06_DesplazamientoEnSentidoContrario {
 
 	public static void main(String[] args) {
 		
-
+		/*
+		 * No he conseguido sacarlo , tenia la idea de como cogiendo una variable y dandole valor 1 o 2 y dependiendo 
+		 * hacia la secuencia a un lado o a otro , para la derecha si lo sabia porque lo hice en el anterior 
+		 * pero a la izquierda no he conseguido sacarlo (ya que entendia que envez de sumar posiciones tenia que restar pero con
+		 * la forma que he hecho yo el ejercicio no me salia restando )
+		 * 
+		 * Ya mañana en clase pregunto alguno de los 2 por curiosidad para saber como se hacia :) 
+		 * */
 	}
 
 }
