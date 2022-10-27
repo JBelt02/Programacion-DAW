9f7284f85caa8e79db99cba588ae9bedca874dcc
JBelt02 <granjuan02@gmail.com>
2022-10-27 13:00:56 +0200

    feat ()

--- src/Capitulo04/bloque04/Ejercicio01_ParesEImparesPosiciones.java
+++ src/Capitulo04/bloque04/Ejercicio01_ParesEImparesPosiciones.java
@@ -1,5 +1,37 @@
 package Capitulo04.bloque04;
 
-public class Ejercicio01_ParesEImparesPosiciones {
+import Utils.arrays;
 
+public class Ejercicio01_ParesEImparesPosiciones {
+	public static void main (String args[]) {
+		int[] numeros = arrays.creaArrayNumerosAzar(20, 0, 100);
+		int[] numeros2 = new int[20];
+		int i = 0 ; 
+		int aux = 0 ;
+		boolean cambios = true ; 
+		
+		System.out.println("Lista sin cambios ");
+		arrays.mostrarArray(numeros);
+		
+		for (i = 0; i < numeros.length; i++) { 
+			while(cambios) {
+				cambios = false ; 
+				if(numeros[i] % 2 == 0) {
+					aux = numeros[i];
+					if(numeros2[i] == 0) {
+						numeros2[i] = aux ;
+						cambios = true ; 
+					}else if (numeros2[i] != 0) {
+						
+					}
+				}
+			}
+		}
+		
+		System.out.println(" ");
+		System.out.println("Lista ordenada");
+		System.out.println(" ");
+		arrays.mostrarArray(numeros);
+		
+	}
 }
